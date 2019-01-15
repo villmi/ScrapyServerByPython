2c49d75b8d713f91593000254598e8c6da3a6557
vill <544912473@qq.com>
2019-01-15 17:53:54 +0800

    这是一个无聊彩蛋，可以用来查询成绩，具体格式是 scrapy crawl query_chenji -a keyword=学号,密码

---
+++ Eggs/chengji/chengji/spiders/query.py
@@ -0,0 +1,48 @@
+import scrapy
+
+
+class QuerySpider(scrapy.Spider):
+    name = "query_chenji"
+
+    def start_requests(self):
+        url = 'http://210.28.80.133:8080/pyxx/login.aspx'
+        tag = getattr(self, 'keyword', None)
+        tag = str(tag).split(',')
+        username = tag[0]
+        password = tag[1]
+        yield scrapy.Request(url=url, callback=self.parse, meta={'username': username, 'password': password})
+
+    def parse(self, response):
+        url = 'http://210.28.80.133:8080/pyxx/login.aspx'
+        viewState = response.xpath('/html/body/form/input[@name="__VIEWSTATE"]/@value').extract_first()
+        print(viewState)
+        yield scrapy.FormRequest(url=url, callback=self.parse2, formdata={'__VIEWSTATE': 'dDwyMTQxMjc4NDIxOztsPF9jdGwwOkltYWdlQnV0dG9uMTtfY3RsMDpJbWFnZUJ1dHRvbjI7Pj4k81flOCblpdLMrVwk7oAsLQ+YHg==',
+                                                                          '_ctl0:txtusername': response.meta['username'],
+                                                                          '_ctl0:ImageButton1.x': '0',
+                                                                          '_ctl0:ImageButton1.y': '0',
+                                                                          '_ctl0:txtpassword': response.meta['password']})
+
+    def parse1(self, response):
+        url = 'http://yjsc.njue.edu.cn:8080/pyxx/leftmenu.aspx'
+        print('redirect to leftMenu .....')
+        print(response.request.headers.getlist('Cookie'))
+        with open("save1.html", 'w', encoding='GBK') as f:
+            f.write(response.text)
+            f.close()
+        yield scrapy.Request(url=url, callback=self.parse2, cookies={'ASP.NET_SessionId': 'poknxb55if5pl4552iivjkzu'})
+
+    def parse2(self, response):
+        Cookie2 = response.request.headers.getlist('Cookie')
+        cookie = str(Cookie2[0]).replace('b', '').replace('\'', '').split('=')
+        print(cookie[0])
+        print(cookie[1])
+        url2 = 'http://yjsc.njue.edu.cn:8080/pyxx/grgl/xskccjcx.aspx'
+        with open("save2.html", 'w', encoding='GBK') as f:
+            f.write(response.text)
+            f.close()
+        yield scrapy.Request(url=url2, callback=self.parse3, cookies={'ASP.NET_SessionId': cookie[1]})
+
+    def parse3(self, response):
+        with open("save4.html", 'w', encoding='GBK') as f:
+            f.write(response.text)
+            f.close()
