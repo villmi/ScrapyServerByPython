dc130775aedf1c89962132cf822c9535bd4df1a2
vill <544912473@qq.com>
2019-01-24 15:36:31 +0800

    把debug信息给删除了，仍然存在偶尔查询不成功的现象，先告一段落吧，求大佬帮助

--- Eggs/chengji/chengji/settings.py
+++ Eggs/chengji/chengji/settings.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import random
 # Scrapy settings for chengji project
 #
 # For simplicity, this file contains only settings considered important or
@@ -8,6 +8,18 @@
 #     https://doc.scrapy.org/en/latest/topics/settings.html
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+USER_AGENT_LIST = [
+    'MSIE (MSIE 6.0; X11; Linux; i686) Opera 7.23',
+    'Opera/9.20 (Macintosh; Intel Mac OS X; U; en)',
+    'Opera/9.0 (Macintosh; PPC Mac OS X; U; en)',
+    'iTunes/9.0.3 (Macintosh; U; Intel Mac OS X 10_6_2; en-ca)',
+    'Mozilla/4.76 [en_jp] (X11; U; SunOS 5.8 sun4u)',
+    'iTunes/4.2 (Macintosh; U; PPC Mac OS X 10.2)',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:5.0) Gecko/20100101 Firefox/5.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:9.0) Gecko/20100101 Firefox/9.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20120813 Firefox/16.0',
+    'Mozilla/4.77 [en] (X11; I; IRIX;64 6.5 IP30)',
+    'Mozilla/4.8 [en] (X11; U; SunOS; 5.7 sun4u)']
 
 BOT_NAME = 'chengji'
 
@@ -88,3 +100,4 @@ ROBOTSTXT_OBEY = False
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+USER_AGENT= random.choice(USER_AGENT_LIST)
--- Eggs/chengji/chengji/spiders/query_score.py
+++ Eggs/chengji/chengji/spiders/query_score.py
@@ -15,28 +15,41 @@ class QuerySpider(scrapy.Spider):
     def parse(self, response):
         url = 'http://210.28.80.133:8080/pyxx/login.aspx'
         viewState = response.xpath('/html/body/form/input[@name="__VIEWSTATE"]/@value').extract_first()
-        print(viewState)
         yield scrapy.FormRequest(url=url, callback=self.parse1, formdata={'__VIEWSTATE': viewState,
                                                                           '_ctl0:txtusername': response.meta['username'],
                                                                           '_ctl0:ImageButton1.x': '0',
                                                                           '_ctl0:ImageButton1.y': '0',
-                                                                          '_ctl0:txtpassword': response.meta['password']})
+                                                                          '_ctl0:txtpassword': response.meta['password']},
+                                 meta={'username': response.meta['username'], 'password': response.meta['password']})
 
     def parse1(self, response):
         Cookie2 = response.request.headers.getlist('Cookie')
         cookie = str(Cookie2[0]).replace('b', '').replace('\'', '').split('=')
-        print(cookie[0])
-        print(cookie[1])
         url2 = 'http://yjsc.njue.edu.cn:8080/pyxx/grgl/xskccjcx.aspx'
         with open("save2.html", 'w', encoding='GBK') as f:
             f.write(response.text)
             f.close()
-        yield scrapy.Request(url=url2, callback=self.parse2, cookies={'ASP.NET_SessionId': cookie[1]})
+        yield scrapy.Request(url=url2, callback=self.parse2, cookies={'ASP.NET_SessionId': cookie[1]},
+                             meta={'username': response.meta['username'], 'password': response.meta['password']})
 
     def parse2(self, response):
         name = response.xpath('//span[@id = "lblxm"]')
-        name = name.xpath('string(.)').extract()[0]
-        print(name)
-        with open("%s_成绩.html" % name, 'w', encoding='GBK') as f:
-            f.write(response.text)
-            f.close()
+        try:
+            name = name.xpath('string(.)').extract()[0]
+            with open("%s_成绩.html" % name, 'w', encoding='GBK') as f:
+                f.write(response.text)
+                f.close()
+        except:
+            print('failed')
+            Cookie = response.request.headers.getlist('Cookie')
+            print(Cookie)
+            url = 'http://210.28.80.133:8080/pyxx/login.aspx'
+            yield scrapy.Request(url=url, callback=self.parse, meta={'username': response.meta['username'], 'password': response.meta['password']})
+
+
+
+
+
+
+
+
